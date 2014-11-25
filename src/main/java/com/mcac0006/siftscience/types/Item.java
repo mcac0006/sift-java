53539c9fa5f64e35af410e7470edbee9110f41e2
Matthew Cachia <matthew.cachia@gmail.com>
2014-11-25 07:03:15 +0100

    Illegal character in one of the comments, causing the build to fail. Fixed.

--- src/main/java/com/mcac0006/siftscience/types/Item.java
+++ src/main/java/com/mcac0006/siftscience/types/Item.java
@@ -71,7 +71,7 @@ public class Item {
 	private String brand;
 	
 	/**
-	 * Name of the item’s manufacturer.
+	 * Name of the item's manufacturer.
 	 */
 	@JsonProperty("$manufacturer")
 	private String manufacturer;
